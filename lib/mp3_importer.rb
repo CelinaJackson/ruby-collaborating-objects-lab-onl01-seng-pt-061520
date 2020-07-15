f895beb1f6c83006ea589c0a8756e3ce7a1291e6
Celina Jackson <celinanjackson@gmail.com>
2020-07-15 22:44:40 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,10 @@
 class MP3Importer
-  attr_accessor :path 
+  attr_accessor :path
 
   def initialize(path)
-    @path = path 
+    @path = path
   end
-  
+
   def import(list_of_filenames)
     list_of_filenames.each {|filename| Song.new_by_filename(filename)}
   end
