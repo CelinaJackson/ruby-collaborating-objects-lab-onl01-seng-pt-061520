6e76786ff72e08325f7b9adc1f2a08941c8b5b18
Celina Jackson <celinanjackson@gmail.com>
2020-07-15 22:35:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,17 @@ class Song
   attr_accessor :name, :artist
 
   @@all = [ ]
-  def initialize(name)
+  def initialize(name, artist)
     @name = name
+    @artist = artist
+    save
   end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
 end
