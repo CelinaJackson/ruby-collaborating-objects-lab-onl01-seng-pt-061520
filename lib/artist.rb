0c1c6ba8247ecad54412573ce324e1ab2d0c9a09
Celina Jackson <celinanjackson@gmail.com>
2020-07-15 23:05:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,12 +8,12 @@ class Artist
     @songs = []
   end
 
-  def self.all
-    @@all
-  end
-
   def add_song(song)
     self.songs << song
+  end
+
+  def self.all
+    @@all
   end
 
   def self.find_or_create_by_name(name)
